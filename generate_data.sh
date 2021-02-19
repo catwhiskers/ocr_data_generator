988629845905a00d8929e7fd8588ed6ca077222e
EC2 Default User <ec2-user@ip-172-16-70-78.us-west-2.compute.internal>
2021-02-19 10:02:28 +0000

    initial commit

---
+++ generate_data.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+train_count=1000
+
+#zh
+rm -rf "images/train_cn"
+#trdg \
+#-c $train_count -l cn -i '/home/ec2-user/SageMaker/ESD/source/data_generation/data/ZH_1.txt' -na 2 \
+#-b 1 --output_dir "images/train_cn" --font_dir "./font/"
+
+trdg \
+-c $train_count -l cn -i '/home/ec2-user/SageMaker/ESD/source/data_generation/data/ZH_1.txt' -na 2 \
+-b 1 --output_dir "images/train_cn" -ft "./setofont/setofont.ttf"
+
+#en
+rm -rf "images/train_en"
+trdg \
+-c $train_count -l cn -i './data/EN_1.txt' -na 2 -b 1 \
+--output_dir "images/train_en" -ft "./font/Aller_Bd.ttf"
+
+#move raw images
+python convert.py
+
+#mkdir -p ocr_train
+#mkdir -p ocr_train/train 
+#cp output/images/*.jpg ocr_train/train 
+#cp labels.txt ocr_train/
+
